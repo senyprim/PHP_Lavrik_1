73768af3f08f6576b3654d8cbf082e1fbabbe44e
seny <seny_ussur@mail.ru>
2021-07-05 21:50:26 +1000

    при несуществующей категории выпадает ошибка 404

--- hw4/controllers/index.php
+++ hw4/controllers/index.php
@@ -1,27 +1,26 @@
 <?php
+$title = 'Все категории';
+$validCategory = false;
 
-$title='Все категории';
-$id_category=$_GET['id_category']??null;//Берем id_category
+$id_category = $_GET['id_category'] ?? null; //Берем id_category
 $validCategory = !!$id_category //Валидируем
     ? checkCategoryId($id_category) && existCategory($id_category)
     : false;
 
-$category = $validCategory?getCategory(intval($id_category)):null;//Загружаем категорию
+$category = $validCategory ? getCategory(intval($id_category)) : null; //Загружаем категорию
+//Если id_category нет
+if ($id_category === null) {
+    $articles = getAllArticle();
+}
+//Если id_category существующая
+if (!empty($category)) {
+    $articles = getAllArticleByCategory(intval($id_category));
+    $title = 'Категория ' . $category['name'];
+}
 
-$articles = !!$category //Загружаем articles
- ?getAllArticleByCategory(intval($id_category))
- :getAllArticle(); //Берем статьи с базы
-
-$title = !!$category
-?'Категория '.$category['name']
-:'Все категории';
-    
-
-
-$content=render('articles/article-list',[
-    'articles'=>$articles,
-    'title'=>$title
-]);
-
-
-
+$content = $validCategory || $id_category === null
+    ? render('articles/article-list', [
+        'articles' => $articles,
+        'title' => $title
+    ])
+    : render('errors/404');
